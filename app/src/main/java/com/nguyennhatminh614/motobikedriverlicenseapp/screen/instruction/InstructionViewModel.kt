19145fe2d149ff200ee97fc1adf3fb75808ccab4
unknown <trieuvino.1@gmail.com>
2022-12-07 23:25:23 +0700

    fix bud instruction screen

--- app/src/main/java/com/nguyennhatminh614/motobikedriverlicenseapp/screen/instruction/InstructionViewModel.kt
+++ app/src/main/java/com/nguyennhatminh614/motobikedriverlicenseapp/screen/instruction/InstructionViewModel.kt
@@ -1,8 +1,17 @@
 package com.nguyennhatminh614.motobikedriverlicenseapp.screen.instruction
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.nguyennhatminh614.motobikedriverlicenseapp.data.repository.InstructionRepository
 import com.nguyennhatminh614.motobikedriverlicenseapp.utils.base.BaseViewModel
 
 class InstructionViewModel: BaseViewModel(){
+    private val _isVisibleInstructionIcon = MutableLiveData(true)
 
+    val isVisibleInstructionIcon : LiveData<Boolean>
+        get() = _isVisibleInstructionIcon
+
+    fun setVisibleInstructionIcon(isVisible: Boolean) {
+        _isVisibleInstructionIcon.value = isVisible
+    }
 }
