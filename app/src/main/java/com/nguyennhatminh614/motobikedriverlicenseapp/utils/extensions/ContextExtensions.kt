b5b8183914dfec543bc5bdd61f745c2c5eff72ea
nguyennhatminh230801 <fcarsenal614@gmail.com>
2023-05-06 05:56:28 +0700

    update code toast, change license type

--- app/src/main/java/com/nguyennhatminh614/motobikedriverlicenseapp/utils/extensions/ContextExtensions.kt
+++ app/src/main/java/com/nguyennhatminh614/motobikedriverlicenseapp/utils/extensions/ContextExtensions.kt
@@ -2,6 +2,7 @@ package com.nguyennhatminh614.motobikedriverlicenseapp.utils.extensions
 
 import android.content.Context
 import android.graphics.Color
+import android.widget.Toast
 import androidx.annotation.ColorRes
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
@@ -15,9 +16,11 @@ val isCurrentLightModeByDefault : Boolean
 fun Context.getResourceColor(@ColorRes colorID: Int)
     = ContextCompat.getColor(this, colorID)
 
-fun getColorWithAlpha(yourColor: Int, alpha: Int): Int {
-    val red: Int = Color.red(yourColor)
-    val blue: Int = Color.blue(yourColor)
-    val green: Int = Color.green(yourColor)
-    return Color.argb(alpha, red, green, blue)
+fun Context.showToast(message: String, isLongDuration: Boolean = false) {
+    Toast.makeText(
+        this,
+        message,
+        if (isLongDuration) Toast.LENGTH_LONG
+        else Toast.LENGTH_SHORT
+    ).show()
 }
